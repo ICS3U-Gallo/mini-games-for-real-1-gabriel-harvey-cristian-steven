5f91d794dbfebd518381ee162a6a3de7b7b0da76
steven <steven.wong26@ycdsbk12.ca>
2024-11-05 10:45:58 -0500

    Update tower-of-hell.py

--- tower-of-hell.py
+++ tower-of-hell.py
@@ -15,7 +15,12 @@ clock = pygame.time.Clock()
 
 mario_x = 500
 mario_y = 400
+mario_speed = 5
+height = 0
+gravity = 5
+ground = True
 death = False
+platforms = ((470, 430, 150, 10))
 # ---------------------------
 
 running = True
@@ -26,6 +31,18 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print(event)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        mario_x -= mario_speed
+    if keys[pygame.K_RIGHT]:
+        mario_x += mario_speed
+    if keys[pygame.K_SPACE]:
+        velocity_y = -10
+        on_ground = False    
+
+    height += gravity
+    mario_y += height
 
     # GAME STATE UPDATES
     # All game math and comparisons happen here
@@ -42,6 +59,9 @@ while running:
     # Tower walls
     pygame.draw.rect(screen, (74, 74, 74), (-30, -20, 50, 1000))
     pygame.draw.rect(screen, (74, 74, 74), (620, -20, 50, 1000))
+
+    # obby
+    pygame.draw.rect(screen, (113, 122, 60), (470, 430, 150, 10))
 
     # of the game loop
     pygame.display.flip()
