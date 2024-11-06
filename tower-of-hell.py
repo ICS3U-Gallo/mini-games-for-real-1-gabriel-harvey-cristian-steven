8357be04d7e673baab470bd78b9eb41dcc942de0
steven <steven.wong26@ycdsbk12.ca>
2024-11-06 10:56:24 -0500

    Update tower-of-hell.py
    new gameplay and fixed platforms and wall collisions

--- tower-of-hell.py
+++ tower-of-hell.py
@@ -15,13 +15,21 @@ clock = pygame.time.Clock()
 
 mario_x = 500
 mario_y = 400
-mario_speed = 5
-height = 0
+mario_speed = 10
+height2 = 0
 gravity = 5
-ground = True
+ground = False
 death = False
-platforms = ((470, 430, 150, 10))
+platforms = [(470, 430, 150, 10), (275, 400, 150, 10)]
+walls = [(-30, -20, 50, 1000), (620, -20, 50, 1000)] 
 # ---------------------------
+mario_rect = pygame.Rect(mario_x, mario_y, 15, 25)
+for platform in platforms:
+    platform_rect = pygame.Rect(platform) # Hitbox for the obby
+    if mario_rect.colliderect(platform_rect):
+        ground = True # If mario is touching a platform he's on it
+        mario_y = platform_rect.top - mario_rect.height # mario is now on the top of a platform depending on jump or not
+        break # stop the code once it detects that 
 
 running = True
 while running:
@@ -37,12 +45,34 @@ while running:
         mario_x -= mario_speed
     if keys[pygame.K_RIGHT]:
         mario_x += mario_speed
-    if keys[pygame.K_SPACE]:
-        velocity_y = -10
-        on_ground = False    
+    if keys[pygame.K_SPACE] and ground:
+        height2 = -25
+        ground = False    
 
-    height += gravity
-    mario_y += height
+    if not ground:
+        height2 += gravity
+
+    mario_y += height2
+
+    mario_rect = pygame.Rect(mario_x, mario_y, 15, 25)
+
+    ground = False  # assume Mario is in the air
+    for platform in platforms:
+        platform_rect = pygame.Rect(platform)
+        if mario_rect.colliderect(platform_rect) and height2 >= 0:
+            mario_y = platform_rect.top - mario_rect.height 
+            height2 = 0
+            ground = True
+            break
+
+    for wall in walls:
+        wall_rect = pygame.Rect(wall)
+        if mario_rect.colliderect(wall_rect):
+            if mario_rect.right > wall_rect.left and mario_rect.left < wall_rect.left:
+                 mario_x = wall_rect.left - mario_rect.width
+            elif mario_rect.left < wall_rect.right and mario_rect.right > wall_rect.right:
+                 mario_x = wall_rect.right
+
 
     # GAME STATE UPDATES
     # All game math and comparisons happen here
@@ -61,8 +91,8 @@ while running:
     pygame.draw.rect(screen, (74, 74, 74), (620, -20, 50, 1000))
 
     # obby
-    pygame.draw.rect(screen, (113, 122, 60), (470, 430, 150, 10))
-
+    for platform in platforms:
+        pygame.draw.rect(screen, (113, 122, 60), platform)
     # of the game loop
     pygame.display.flip()
     clock.tick(30)
