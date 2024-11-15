eedc910afa4fa533240b9b422a097698a21ffddc
steven <steven.wong26@ycdsbk12.ca>
2024-11-15 18:14:18 -0500

    Update and rename mission-breakout.py to mission_breakout.py
    Added a final level (very easy on purpose), serves as a way to get to your getaway car
    Added some more music
    Finished all of level 3
    Game is now 100% finished

--- mission_breakout.py
+++ mission_breakout.py
@@ -13,6 +13,8 @@ clock = pygame.time.Clock()
 
 # Load sounds
 death_sound = pygame.mixer.Sound("game_over.mp3")
+winner_sound = pygame.mixer.Sound("winner.mp3")
+run = pygame.mixer.Sound("getout.mp3")
 pygame.mixer.music.load("escape.mp3")
 pygame.mixer.music.play(-1)
 
@@ -28,7 +30,7 @@ death_animation = False
 death_velocity = 0
 mario_x = mario_spawn_x
 mario_y = mario_spawn_y
-level = 3
+level = 1
 sweeping_killbrick = None
 sweeping_speed = 10
 sweeping_direction = 1 
@@ -40,7 +42,7 @@ last_toggle_time = pygame.time.get_ticks()
 beat_block_visible = True
 
 def load_stage(level):
-    global platforms, killbricks, walls, mario_y, moving_platform, sweeping_killbrick
+    global platforms, killbricks, walls, mario_y, moving_platform, sweeping_killbrick, beat_blocks
     if level == 1:
         platforms = [
             (470, 430, 150, 10),
@@ -61,6 +63,10 @@ def load_stage(level):
         walls = [(-30, -20, 50, 1000), (620, -20, 50, 1000)]
         moving_platform = None
         sweeping_killbrick = None
+        beat_blocks = []
+        mario_x = mario_spawn_x
+        mario_y = mario_spawn_y
+        
     elif level == 2:
         platforms = [
             (15, 430, 615, 10),
@@ -82,24 +88,90 @@ def load_stage(level):
             (100, 200, 10, 10),
             (110, 220, 20, 10)
         ]
+        mario_x = mario_spawn_x
+        mario_y = mario_spawn_y
         walls = [(-30, -20, 50, 1000), (620, -20, 50, 1000)]
         moving_platform = pygame.Rect(15, 400, 50, 10)
-        sweeping_killbrick = pygame.Rect(200, 220, 50, 10)
+        sweeping_killbrick = pygame.Rect(160, 220, 50, 10)
+        beat_blocks = []
     elif level == 3:
         platforms = [
             (15, 430, 615, 10),
-            (200, 300, 100, 10),
-            (400, 390, 100, 10),
+            (300, 380, 100, 10),
+            (400, 380, 15, 50),
+            (410, 400, 15, 30),
+            (420, 420, 15, 10),
+            (20, 320, 110, 10),
+            (250, 240, 200, 10),
+            (440, 240, 10, 55),
+            (450, 260, 10, 35),
+            (460, 275, 10, 15),
+            (510, 275, 10, 15),
+            (520, 255, 10, 35),
+            (530, 240, 10, 45),
+            (455, 285, 85, 10),
+            (540, 240, 95, 10),
+            (600, 120, 20, 10),
+            (360, 120, 20, 10),
+            (240, 120, 20, 10)
         ]
         killbricks = [
-            (440, 390, 20, 40)
+            (20, 420, 380, 10),
+            (350, 370, 15, 15),
+            (470, 405, 10, 30),
+            (600, 230, 30, 10),
+            (300, 120, 20, 10),
         ]
         moving_platform = None
+        sweeping_killbrick = pygame.Rect(500, 190, 10, 50)
+        beat_blocks = [
+            pygame.Rect(170, 360, 100, 10),
+            pygame.Rect(170, 270, 20, 10),
+            pygame.Rect(600, 180, 20, 10),
+            pygame.Rect(520, 120, 20, 10),
+            pygame.Rect(440, 120, 20, 10),
+            pygame.Rect(240, 60, 20, 10),
+        ]
+        mario_x = mario_spawn_x
+        mario_y = mario_spawn_y
+    elif level == 4:
+        pygame.mixer.music.stop()
+        pygame.mixer.music.load("getout.mp3")
+        pygame.mixer.music.play(-1)
+        platforms = [
+            (15, 430, 425, 10),
+            (245, 160, 90, 50),
+            (245, 200, 40, 100),
+            (275, 270, 60, 30),
+            (275, 270, 60, 30),
+            (295, 220, 40, 50),
+            (345, 160, 90, 50),
+            (345, 200, 40, 100),
+            (375, 270, 60, 30),
+            (395, 220, 40, 50),
+            (435, 430, 10, 200),
+            (540, 430, 10, 200),
+            (540, 430, 200, 10),
+        ]
+        killbricks = [
+            (235, 150, 100, 10),
+            (235, 300, 100, 10),
+            (235, 150, 10, 150),
+            (285, 210, 50, 10),
+            (285, 220, 10, 50),
+            (335, 150, 100, 10),
+            (335, 300, 110, 10),
+            (335, 150, 10, 150),
+            (435, 150, 10, 150),
+            (385, 210, 50, 10),
+            (385, 220, 10, 50)
+        ] + [(x, 415, 25, 25) for x in range(300, 400, 80)]
+        mario_x = mario_spawn_x-200
+        mario_y = mario_spawn_y
         sweeping_killbrick = None
-
-    mario_x = mario_spawn_x
-    mario_y = mario_spawn_y
+        beat_blocks = []
     walls = [(-30, -20, 50, 1000), (620, -20, 50, 1000)]
+
 
 
 # Load the initial stage
@@ -137,10 +209,12 @@ while running:
             last_toggle_time = current_time
 
         ground = False  # Assume Mario is in the air
-        for platform in platforms + ([moving_platform] if moving_platform else []):
+        # i am also including platforms and visible beat blocks in the collision check
+        for platform in platforms + ([moving_platform] if moving_platform else []) + (
+                [pygame.Rect(bb) for bb in beat_blocks] if beat_block_visible else []):
             platform_rect = pygame.Rect(platform)
             if mario_rect.colliderect(platform_rect) and velocity_y >= 0:
-                mario_y = platform_rect.top - mario_rect.height 
+                mario_y = platform_rect.top - mario_rect.height
                 velocity_y = 0
                 ground = True
                 break
@@ -166,6 +240,11 @@ while running:
         if level == 2 and sweeping_killbrick:
             sweeping_killbrick.x += sweeping_speed * sweeping_direction
             if sweeping_killbrick.left <= 150 or sweeping_killbrick.right >= 610:
+                sweeping_direction *= -1
+
+        if level == 3 and sweeping_killbrick:
+            sweeping_killbrick.x += sweeping_speed * sweeping_direction
+            if sweeping_killbrick.left <= 250 or sweeping_killbrick.right >= 615:
                 sweeping_direction *= -1
 
         if sweeping_killbrick and mario_rect.colliderect(sweeping_killbrick):
@@ -197,6 +276,16 @@ while running:
         if moving_platform.y < -moving_platform.height:
             moving_platform.y = HEIGHT
 
+    if level == 4:
+        car_rect = pygame.Rect(545, 390, 45, 25)  
+        if mario_rect.colliderect(car_rect):
+            winner_sound.play() 
+            mario_x = 0
+            mario_y = 0
+            velocity_y = 0
+            pygame.time.delay(3000)
+            running = False
+
     # Drawing
     screen.fill((36, 40, 64))
 
@@ -224,7 +313,13 @@ while running:
 
     if beat_block_visible:
         for beat_block in beat_blocks:
-            pygame.draw.rect(screen, (0, 128, 255), beat_block)
+            pygame.draw.rect(screen, (90,90,90), beat_block)
+
+    if level == 4:
+        pygame.draw.rect(screen, (66, 66, 66), (545, 390, 45, 25))
+        pygame.draw.rect(screen, (100,100,100), (590, 400, 15, 15))
+        pygame.draw.circle(screen, (24, 24, 24), (590, 420), 7.5)
+        pygame.draw.circle(screen, (24, 24, 24), (550, 420), 7.5)
 
     pygame.draw.rect(screen, (66, 66, 66), (mario_x, mario_y, 15, 15))
     pygame.draw.rect(screen, (247, 232, 119), (mario_x + 2.5, mario_y - 10, 10, 10))
